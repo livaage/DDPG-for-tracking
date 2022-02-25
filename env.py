Only one variable, fixed gradient saturating

env.py
@@ -39,11 +39,7 @@ class TrackEnv():
     
     def __init__(self): 
         self.file_number = 0 
-        self.initial_event = pd.read_hdf(config['input_dir']+config['file_name']+str(self.file_number)+config['file_extension'])
-        
-        df = self.initial_event[self.initial_event['sim_pt'] > 2]
-        self.event = df
-        #self.event = pt_cut
+
         self.episode_counter = 0 
         self.record_partilce_ids = [] 
         self.record_r = [] 
@@ -53,110 +49,76 @@ class TrackEnv():
         self.record_new_r = []
         self.record_new_z = [] 
         self.previous_state = []
+        self.record_original_pid = [] 
         self.reset_count = 0 
         self.write = 2 
     #completes one step based on the input action value 
     def step(self, a): 
        
-        dr = a[0]
-        dz = a[1]
-
+        #dr = a[0]
+        #dz = a[1]
         
-        new_r = self.state.r + dr 
+        new_r = self.state.r + a 
         #checking that the new x is within the x limits
-        new_r = np.clip(new_r, 0, 25.4)
+        
+        new_r = np.clip(new_r, 0, 26)
 
-        new_z = self.state.z + dz 
-        new_z = np.clip(new_z, -266, 266)
-        #if (self.episode_counter > 2800) & (self.episode_counter < 2850): 
+        #new_z = a[1]
+        #new_z = self.state.z + dz 
+        #new_z = np.clip(new_z, -265, 265)
+
+# stores and writes result to file         
+        if (self.episode_counter > 2800) & (self.episode_counter < 2850): 
             #print("episode counter", self.episode_counter)
-        self.record_partilce_ids.append(self.state.particle_id)
-        self.record_r.append(self.state.r)
-        self.record_dr.append(dr)
-        self.record_z.append(self.state.z)
-        self.record_dz.append(dz)
-        self.record_new_r.append(new_r)
-        self.record_new_z.append(new_z)
-        
+            self.record_partilce_ids.append(self.state.particle_id)
+            self.record_r.append(self.state.r)
+            #self.record_dr.append(dr)
+            #self.record_z.append(self.state.z)
+            #self.record_dz.append(dz)
+            self.record_new_r.append(new_r)
+            #self.record_new_z.append(new_z)
+            self.record_original_pid.append(self.original_pid)
+
     
-        #if (self.episode_counter == 2850) & (self.write==2): 
-        np.savetxt('pids.csv', self.record_partilce_ids, delimiter=',')
-        np.savetxt('rs.csv', self.record_r, delimiter=',')
-        np.savetxt('drs.csv', self.record_dr, delimiter=',')
-        np.savetxt('zs.csv', self.record_z, delimiter=',')
-        np.savetxt('dzs.csv', self.record_dz, delimiter=',')
-        np.savetxt('new_r.csv', self.record_new_r, delimiter=',')
-        np.savetxt('new_z.csv', self.record_new_z, delimiter=',')
+        if (self.episode_counter == 3000) & (self.write==2): 
+            np.savetxt('pids.csv', self.record_partilce_ids, delimiter=',')
+            np.savetxt('rs.csv', self.record_r, delimiter=',')
+            #np.savetxt('drs.csv', self.record_dr, delimiter=',')
+            np.savetxt('zs.csv', self.record_z, delimiter=',')
+            #np.savetxt('dzs.csv', self.record_dz, delimiter=',')
+            np.savetxt('new_r.csv', self.record_new_r, delimiter=',')
+            #np.savetxt('new_z.csv', self.record_new_z, delimiter=',')
+            np.savetxt('original_pid.csv', self.record_original_pid, delimiter=',')
 
-           # self.write = 1 
-        #self.event = self.event[self.event['sim_pt']>2]
 
-    #ensuring the new hit picked can't be the same as the hit under consideration 
-        other_hits = self.event[self.event['hit_id']!= self.state.hit_id]
-        #find hits in the new x region (update to y and z)
+        #dangerous - relies on an ordered df 
+        next_hit = self.original_particle.loc[self.num_track_hits +1,: ]
 
-        # this makes it faster to search for closest hit later on 
-        compatible_hits = other_hits[(other_hits['r'] > (new_r -3)) & (other_hits['r'] < (new_r +3)) & (other_hits['z'] > (new_z - 5)) & (other_hits['z'] < (new_z + 5))]
-        same_particle = compatible_hits[compatible_hits['particle_id']==self.original_pid]
-        contains_same_track = len(same_particle) > 0 
-        
-        if contains_same_track: 
-            distances = [] 
-            #it's a big search, converting to list from pandas save an order of magnitude in time 
-            rlist = same_particle.r.tolist()
-            zlist = same_particle.z.tolist() 
-            for ix in range(len(same_particle)): 
-                row = same_particle.iloc[ix, ]
-                distance = np.abs(new_r - rlist[ix]) + np.abs(new_z-zlist[ix])
-                distances.append(distance)
-            # the reward is 1/lenght of distance between projected place and the closest hit for that particle 
-            reward = 1/min(distances)
-            #reward = 1 
-        else: 
-            reward = -1
-        
-        if len(compatible_hits) == 0: 
-            
-            #return  np.array([self.state.r, self.state.z, self.previous_state.r, self.previous_state.z]), -1, True
-            compatible_hits =  other_hits[(other_hits['r'] > (new_r - 10)) & (other_hits['r'] < (new_r + 10)) & (other_hits['z'] > (new_z - 100)) & (other_hits['z'] < (new_z + 100))]
-            print("using full hits")
-        # update the track to the hit with the closest r state 
-        #closest_r_hit_idx = np.argmin(np.abs(compatible_hits.r - new_r))
-        distances = [] 
-        #it's a big search, converting to list from pandas save an order of magnitude in time 
-        rlist = compatible_hits.r.tolist()
-        zlist = compatible_hits.z.tolist() 
-        for ix in range(len(compatible_hits)): 
-            row = compatible_hits.iloc[ix, ]
-            distance = np.abs(new_r - rlist[ix]) + np.abs(new_z-zlist[ix])
-            distances.append(distance)
-        new_hit = compatible_hits.iloc[np.argmin(distances), ]
-      #  if new_hit.particle_id == self.original_pid: 
-      #      reward =1
-      #  else: 
-      #      reward = -1 
+        #distance_next_hit = np.sqrt((new_r-next_hit.r)**2 + (new_z - next_hit.z)**2)
+        distance_next_hit = np.abs(new_r-next_hit.r)
+        #print(distance_next_hit)
+        reward = -distance_next_hit
+
+        new_hit = next_hit
         
  
 
-      #  if new_hit.particle_id == self.state.particle_id: 
-      #      reward = 1 
-      #  else: 
-      #      reward = -1 
-
-        #new_hit = compatible_hits.iloc[closest_r_hit_idx]
-        
         self.previous_state = self.state 
         self.state = new_hit 
         self.num_track_hits += 1 
+        phi = self.calculate_phi(new_hit.hit_id)
 
-        if self.num_track_hits > 4:
-            done = True
+        dr = self.state.r - self.previous_state.r 
+        #dz = self.state.z - self.previous_state.z 
+
+        if self.num_track_hits > 6:
+            done = True 
         else: 
             done = False 
             self.episode_counter +=1 
 
 
-        state = np.array([self.state.r, self.state.z, self.previous_state.r, self.previous_state.z])
+        state = np.array([self.state.r, dr, self.state.z])
 
         return state, reward, done
         
@@ -166,23 +128,63 @@ class TrackEnv():
         # start at random hit, but could start arbitrarly 
         #print("lenght event", len(self.event))
         #print("hit id", self.event.hit_id.values)
-        random_hit_id = random.choice(self.event.hit_id.values)
-        #print("hte hit id is ", random_hit_id)
-        random_hit = self.event[self.event['hit_id']==random_hit_id]
-        self.original_pid = random_hit.particle_id.values[0]
-        np.savetxt('original_pid.csv', [self.original_pid], delimiter=',')
+        self.initial_event = pd.read_hdf(config['input_dir']+config['file_name']+str(self.file_number)+config['file_extension'])
+        self.event = self.initial_event[self.initial_event['sim_pt'] > 2]
+        #subset by the number of hits 
+        nh = self.event.groupby('particle_id').agg('count').iloc[:,0]
+        # only pick the pids that has a certain number of hits 
+        self.event = self.event[self.event['particle_id'].isin(np.array(nh[nh > 7].index))]
 
-        #this turns it into a series instead of a df 
-        self.state = random_hit.squeeze(axis=0)
-        self.last_dr = None 
-        self.num_track_hits = 0 
-        self.reset_count += 1 
         if self.reset_count > 100: 
             self.file_number += 1 
             self.initial_event = pd.read_hdf(config['input_dir']+config['file_name']+str(self.file_number)+config['file_extension'])
             self.event = self.initial_event[self.initial_event['sim_pt'] > 2]
+            #subset by the number of hits 
+            nh = self.event.groupby('particle_id').agg('count').iloc[:,0]
+            # only pick the pids that has a certain number of hits 
+            self.event = self.event[self.event['particle_id'].isin(np.array(nh[nh > 7].index))]
             self.reset_count = 0 
             print("jumping to file", self.file_number)
 
-        return np.array([self.state.r, self.state.z, 0, 0])
+        random_particle_id = random.choice(self.event.particle_id.values)
+        self.particle = self.event[self.event['particle_id']==random_particle_id]
+        self.original_pid = random_particle_id
+        start_hit = self.particle.iloc[0:,]
+        #allowed_starts = self.event[self.event['layer_id']==1]
+        #random_hit_id = random.choice(allowed_starts.hit_id.values)
+        #random_hit = self.event[self.event['hit_id']==random_hit_id]
+        
+        #self.original_pid = random_hit.particle_id.values[0]
+        #self.particle = self.event[self.event['particle_id']==self.original_pid].reset_index() 
+        
+        self.index1 = 0
 
+            
+        hit2  = self.particle.iloc[self.index1+1,:]
+        phi2 = self.calculate_phi(hit2.hit_id)
+        #del_phi = self.calculate_phi(random_hit.hit_id.value[0]) - phi2 
+        #print(hit2.z)
+       #dz = start_hit.z.values[0] - hit2.z
+        dr = start_hit.r.values[0] - hit2.r
+
+       
+
+        #this turns it into a series instead of a df 
+        self.state = hit2.squeeze(axis=0)
+
+        self.last_dr = None 
+        self.num_track_hits = 2 
+        self.reset_count += 1 
+        self.original_particle = self.event[self.event['particle_id']==self.original_pid].reset_index()
+        # 
+ 
+
+        return np.array([self.state.r, dr, self.state.z])
+
+    def calculate_phi(self, hit_id): 
+        h = self.event[self.event['hit_id']==hit_id]
+        angle = np.arctan(h.y.values[0]/np.abs(h.x.values[0]))
+
+        if h.x.values[0] < 0: 
+            angle = np.pi - angle 
+        return angle 

globals.py
@@ -19,5 +19,7 @@ def initialise_globals():
     actor_lr = 0.001
     gamma = 0.99
 
-    critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
+    #clipvalue
+
+    critic_optimizer = tf.keras.optimizers.Adam(critic_lr, amsgrad=True)
     actor_optimizer = tf.keras.optimizers.Adam(actor_lr)

learn.py
@@ -11,19 +11,23 @@ import globals
 
 globals.initialise_globals() 
 
-std_dev = 3
-ou_noise = OUActionNoise(mean=np.zeros(2), std_deviation=float(std_dev) * np.ones(2))
+std_dev = 5
+ou_noise1 = OUActionNoise(mean=np.array([0]), std_deviation=float(std_dev) * np.ones(1))
+
+std_dev = 20
+ou_noise2 = OUActionNoise(mean=np.array([0]), std_deviation=float(std_dev) * np.ones(1))
+
 # Discount factor for future rewards
 # Used to update target networks
-tau = 0.8
+tau = 0.005
 
-buffer = Buffer(100, 1024)
+buffer = Buffer(100, 64)
 
 # To store reward history of each episode
 ep_reward_list = []
 # To store average reward history of last few episodes
 avg_reward_list = []
-total_episodes = 1000
+total_episodes = 100000
 # Takes about 4 min to train
 
 env = TrackEnv()
@@ -39,7 +43,7 @@ for ep in range(total_episodes):
 
         tf_prev_state = tf.expand_dims(tf.convert_to_tensor(prev_state), 0)
 
-        action = policy(tf_prev_state, ou_noise)
+        action = policy(tf_prev_state, ou_noise1)
 
         state, reward, done = env.step(action[0])
         

model.py
@@ -7,8 +7,9 @@ with open("config.yaml", "r") as f:
 
 num_states = config['num_states']
 num_actions = config['num_actions']
-#upper_bound = config['upper_bound_r']
-upper_bound = config['upper_bound']
+#
+upper_bound = config['upper_bound_r']
+#upper_bound = config['upper_bound']
 lower_bound = config['lower_bound_r']
 
 def get_actor():
@@ -17,17 +18,23 @@ def get_actor():
 
     inputs = layers.Input(shape=(num_states,))
     out = layers.Dense(256, activation="relu")(inputs)
-    out = layers.Dense(256, activation="relu")(out)
-    outputs = layers.Dense(2, activation="tanh", kernel_initializer=last_init)(out)
+    out = layers.Dense(64, activation="relu")(out)
+
+
+
+
+    outputs = layers.Dense(1, activation="tanh", kernel_initializer=last_init)(out)
 
     # Our upper bound is 2.0 for Pendulum.
-    #outputs = outputs * upper_bound
+    outputs = outputs * upper_bound
     model = tf.keras.Model(inputs, outputs)
     return model
 
 
 def get_critic():
     # State as input
+    first_init = tf.random_uniform_initializer(minval=-1000, maxval=0)
+
     state_input = layers.Input(shape=(num_states))
     state_out = layers.Dense(32, activation="relu")(state_input)
     state_out = layers.Dense(32, activation="relu")(state_out)
@@ -39,9 +46,10 @@ def get_critic():
     # Both are passed through seperate layer before concatenating
     concat = layers.Concatenate()([state_out, action_out])
 
-    out = layers.Dense(256, activation="relu")(concat)
-    out = layers.Dense(256, activation="relu")(out)
-    outputs = layers.Dense(2)(out)
+    out = layers.Dense(32, activation="relu")(concat)
+    out = layers.Dense(32, activation="relu")(out)
+    #out = layers.Rescaling(1/1000)(out)
+    outputs = layers.Dense(1,kernel_initializer=first_init)(out)
 
     # Outputs single value for give state-action
     model = tf.keras.Model([state_input, action_input], outputs)

policy.py
@@ -7,19 +7,22 @@ import globals
 with open("config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-upper_bound = config['upper_bound']
-lower_bound = config['lower_bound']
+upper_bound = config['upper_bound_r']
+lower_bound = config['lower_bound_r']
 
 
-def policy(state, noise_object):
+def policy(state, noise_object1):
     sampled_actions = tf.squeeze(globals.actor_model(state))
-    noise = noise_object()
+    noise1 = noise_object1()
+    #noise2 = noise_object2()
     # Adding noise to action
-    #print("sample 1", sampled_actions)
-    sampled_actions = sampled_actions.numpy() + noise
+    sampled_actions1 = sampled_actions.numpy() + noise1 
+    #sampled_actions1 = sampled_actions.numpy()[0] + noise1 
+    #sampled_actions2 = sampled_actions.numpy()[1] + noise2
+    #sampled_actions = np.array([sampled_actions1, sampled_actions2]).flatten()
     #print("sample2", sampled_actions)
     #print()
     # We make sure action is within bounds
-    legal_action = np.clip(sampled_actions, lower_bound, upper_bound)
+    legal_action = np.clip(sampled_actions1, lower_bound, upper_bound)
 
     return [np.squeeze(legal_action)]